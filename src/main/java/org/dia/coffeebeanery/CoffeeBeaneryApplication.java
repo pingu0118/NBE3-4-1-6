9efa0850f713aedcbed8cbbddf6db25f2ab1b075
dia218 <sang218@kyonggi.ac.kr>
2024-12-23 00:47:18 +0900

    chore: Init project

---
+++ src/main/java/org/dia/coffeebeanery/CoffeeBeaneryApplication.java
@@ -0,0 +1,13 @@
+package org.dia.coffeebeanery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeBeaneryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeBeaneryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/dia/coffeebeanery/CoffeeBeaneryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dia.coffeebeanery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeBeaneryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
