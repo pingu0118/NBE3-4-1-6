195a99a786e3831facdcc7083251c074c0370a22
dia218 <sang218@kyonggi.ac.kr>
2025-01-08 15:30:19 +0900

    feat(SellerProduct): Implement Product Seller Controller
    - implement SellerProductController.
    - implement SellerProductService.
    
    - testing not yet.

--- src/main/java/org/team6/coffeebeanery/product/controller/SellerProductController.java
+++ src/main/java/org/team6/coffeebeanery/product/controller/SellerProductController.java
@@ -1,12 +1,34 @@
 package org.team6.coffeebeanery.product.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.*;
+import org.team6.coffeebeanery.product.model.Product;
 import org.team6.coffeebeanery.product.service.SellerProductService;
-import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
 public class SellerProductController {
     private final SellerProductService sellerProductService;
     
+    @GetMapping({"/seller/products"}) //판매자용) 상품 목록 페이지
+    public Page<Product> getAllProductList(@RequestParam(value = "page", defaultValue = "0") int page) {
+        return this.sellerProductService.getAllProductList(page);
+    }
+    
+    @PostMapping("/seller/products") //판매자용) 새 상품 생성
+    public Product createProduct(@Valid @RequestBody Product product) {
+        return sellerProductService.createProduct(product);
+    }
+    
+    @PutMapping("/seller/products/{productId}") //판매자용) 기존 상품 정보 수정
+    public Product updateProduct(@PathVariable Integer productId, @Valid @RequestBody Product updatedProduct) {
+        return sellerProductService.updateProduct(productId, updatedProduct);
+    }
+    
+    @DeleteMapping("/seller/products/{productId}") //판매자용) 상품 삭제
+    public void deleteProduct(@PathVariable Integer productId) {
+        sellerProductService.deleteProduct(productId);
+    }
 }
--- src/main/java/org/team6/coffeebeanery/product/service/SellerProductService.java
+++ src/main/java/org/team6/coffeebeanery/product/service/SellerProductService.java
@@ -1,6 +1,11 @@
 package org.team6.coffeebeanery.product.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.team6.coffeebeanery.common.exception.ResourceNotFoundException;
+import org.team6.coffeebeanery.product.model.Product;
 import org.team6.coffeebeanery.product.repository.ProductRepository;
 import org.springframework.stereotype.Service;
 
@@ -9,4 +14,32 @@ import org.springframework.stereotype.Service;
 public class SellerProductService {
     private final ProductRepository productRepository;
     
+    public Page<Product> getAllProductList(int page) {
+        Pageable pageable = PageRequest.of(page, 10);
+        return this.productRepository.findAll(pageable);
+    }
+    
+    public Product createProduct(Product product) {
+        return this.productRepository.save(product);
+    }
+    
+    public Product updateProduct(Integer productId, Product updatedProduct) {
+        Product existingProduct = productRepository.findById(productId)
+                                                   .orElseThrow(() -> new ResourceNotFoundException(
+                                                           "Product not found - id: " + productId));
+        
+        existingProduct.setProductName(updatedProduct.getProductName());
+        existingProduct.setProductDescription(updatedProduct.getProductDescription());
+        existingProduct.setProductPrice(updatedProduct.getProductPrice());
+        existingProduct.setProductImageURL(updatedProduct.getProductImageURL());
+        existingProduct.setProductStock(updatedProduct.getProductStock());
+        
+        return productRepository.save(existingProduct);
+    }
+    
+    public void deleteProduct(Integer productId) {
+        Product product = productRepository.findById(productId)
+                                           .orElseThrow(() -> new ResourceNotFoundException("Product not found - id: " + productId));
+        productRepository.delete(product);
+    }
 }
